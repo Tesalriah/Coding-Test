9cf9d41afb825006d20924f11992c681a5d8df9c
Tesalriah <95607242+Tesalriah@users.noreply.github.com>
2024-04-19 18:10:15 +0900

    [Bronze V] Title: 새싹, Time: 124 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,14 @@
+public class Main{
+    public static void main(String[] args) {
+        String str = "";
+
+        str += "         ,r'\"7\n";
+        str += "r`-_   ,'  ,/\n";
+        str += " \\. \". L_r'\n";
+        str += "   `~\\/\n";
+        str += "      |\n";
+        str += "      |\n";
+
+        System.out.print(str);
+    }
+}
