b14b0bd35f4ea65e434a94de91d4c28efe053399
GeunHwi Bae <95607242+Tesalriah@users.noreply.github.com>
2025-10-14 20:55:29 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 0;
+
+        while (true){
+            if(n % 5 == 0){
+                cnt += n / 5;
+                System.out.println(cnt);
+                break;
+            }
+            n -= 3;
+            cnt++;
+
+            if(n<0){
+                System.out.println(-1);
+                break;
+            }
+        }
+    }
+}
