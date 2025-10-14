4215d5965a33d99fe91fb2595e87f69c6b22ed41
GeunHwi Bae <95607242+Tesalriah@users.noreply.github.com>
2025-10-14 18:51:49 +0900

    [Silver V] Title: 영화감독 숌, Time: 272 ms, Memory: 85168 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        int num = 0;
+
+        while (cnt != N){
+            num++;
+            String str = num + "";
+            if(str.contains("666")){
+                cnt++;
+            }
+        }
+
+        System.out.println(num);
+    }
+}
