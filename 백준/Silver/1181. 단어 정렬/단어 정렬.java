52d33708f354d77fd5b1ad5dc721044f6d98ca87
GeunHwi Bae <95607242+Tesalriah@users.noreply.github.com>
2025-10-25 02:19:44 +0900

    [Silver V] Title: 단어 정렬, Time: 360 ms, Memory: 24968 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,41 +1,32 @@
-import java.util.Scanner;
+import java.io.*;
 import java.util.Arrays;
 import java.util.Comparator;
- 
- 
+
 public class Main {
-	public static void main(String[] args) {
-    
-		Scanner in = new Scanner(System.in);
- 
-		int N = in.nextInt();
-		String[] arr = new String[N];
- 
-		in.nextLine();
- 
-		for (int i = 0; i < N; i++) {
-			arr[i] = in.nextLine();
-		}
-		
-		Arrays.sort(arr, new Comparator<String>() {
-			public int compare(String s1, String s2) {
-				if (s1.length() == s2.length()) {
-					return s1.compareTo(s2);
-				} 
-				else {
-					return s1.length() - s2.length();
-				}
-			}
-		});
- 
- 
-		System.out.println(arr[0]);
-		
-		for (int i = 1; i < N; i++) {
-			if (!arr[i].equals(arr[i - 1])) {
-				System.out.println(arr[i]);
-			}
-		}
-	}
- 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n =  Integer.parseInt(br.readLine());
+        String[] arr = new String[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = br.readLine();
+        }
+
+        Arrays.sort(arr);
+        Arrays.sort(arr, Comparator.comparing(String::length));
+
+        String temp = "";
+        for(int i = 0; i < n; i++){
+            if(temp.equals(arr[i])){
+                continue;
+            }else{
+                bw.write(arr[i] + "\n");
+                temp = arr[i];
+            }
+        }
+        bw.flush();
+        bw.close();
+    }
 }
