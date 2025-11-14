86332991e46fb51ef4a6883a0bfb47d82323ada3
GeunHwi Bae <95607242+Tesalriah@users.noreply.github.com>
2025-11-14 23:59:52 +0900

    [Silver III] Title: 분수 합, Time: 124 ms, Memory: 16028 KB -BaekjoonHub

--- 백준/Silver/1735. 분수 합/분수 합.java
+++ 백준/Silver/1735. 분수 합/분수 합.java
@@ -1,29 +1,49 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
-	
-	public static void main(String[] args) {
-		Scanner scan= new Scanner(System.in);
-		int c1, c2, p1, p2;
-		
-		//c1/p1, c2/p2
-		c1= scan.nextInt();
-		p1= scan.nextInt();
-		c2= scan.nextInt();
-		p2= scan.nextInt();
-		
-        //더해주고
-		c1=c1*p2+c2*p1;
-		p1=p1*p2;
-		int gcd= getGcd(c1, p1); //최대공약수를 구해준다.
-		
-		System.out.println(c1/gcd+" "+p1/gcd); //기약분수 출력
-	}//main
-	
-	public static int getGcd(int a, int b) {
-		if(a%b==0) {
-			return b;
-		}
-		return getGcd(b, a%b);
-	}
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int C = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
+
+        int numerator = A * D + B * C;
+        int denominator = B * D;
+        
+        int mod = gcd(numerator, denominator);
+        numerator /= mod;
+        denominator /= mod;
+
+        bw.write(numerator + " " + denominator + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    // 유클리드 호제법
+    public static int gcd(int a, int b) {
+        // a가 b보다 큰 경우에 대해서 유클리드 호제법 수행.
+        if (a <= b) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b , a % b);
+    }
+
 }
