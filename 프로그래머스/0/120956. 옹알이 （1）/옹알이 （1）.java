eab0dbed298f08743a4401b3fc3f2eba5e7053db
GeunHwi Bae <95607242+Tesalriah@users.noreply.github.com>
2026-03-11 19:32:15 +0900

    [level 0] Title: 옹알이 (1), Time: 0.93 ms, Memory: 90.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        String[] can = {"aya", "ye", "woo", "ma"};
+        
+        for(int k=0; k<babbling.length; k++){
+            String str = babbling[k];
+            String say = babbling[k];
+            int cnt = 0;
+            for(int i=0; i<can.length; i++){
+                say = say.replace(can[i], " ");
+            }
+            if(say.isBlank()){
+                answer++;
+            }    
+        }
+        
+        return answer;
+    }
+}
