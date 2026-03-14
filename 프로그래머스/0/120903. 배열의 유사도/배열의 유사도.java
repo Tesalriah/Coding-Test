5e57ef909b534d3b5688b4eb606ed55dad1fdd6e
GeunHwi Bae <95607242+Tesalriah@users.noreply.github.com>
2026-03-14 13:57:49 +0900

    [level 0] Title: 배열의 유사도, Time: 0.11 ms, Memory: 85.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] s1, String[] s2) {
+        int answer = 0;
+        Set<String> set = new HashSet<String>(Arrays.asList(s2));
+        
+        for(String str : s1){
+            if(set.contains(str)){
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
