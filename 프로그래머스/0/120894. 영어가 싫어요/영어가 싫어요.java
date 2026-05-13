e76ff3aa34af8548a50747bfa732d573d6aede2f
GeunHwi Bae <95607242+Tesalriah@users.noreply.github.com>
2026-05-13 12:18:15 +0900

    [level 0] Title: 영어가 싫어요, Time: 5.17 ms, Memory: 63.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -0,0 +1,12 @@
+class Solution {
+    public long solution(String numbers) {
+        long answer = 0;
+        String[] num = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+       
+        for(int i = 0; i < num.length; i++){
+            numbers = numbers.replace(num[i], i + "");
+        }
+        answer = Long.parseLong(numbers);
+        return answer;
+    }
+}
